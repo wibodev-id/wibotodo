a8a44db017dac8ced8f757a459b10628f6e450ec
WiboDev <wibodev-id@gmail.com>
2026-05-31 00:29:39 +0700

    sign with google

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.todo.wibosystems.com',
-  googleClientId: '',
+  googleClientId: '675458575390-ntesq6tog1eipfq8t31bddo70k9udvhd.apps.googleusercontent.com',
 };
